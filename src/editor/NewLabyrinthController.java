a3ff120a9b16d06f4fe998205b02397051139f29
Marius Schär <schaer.marius@gmail.com>
2016-11-16 14:28:52 +0100

    added savedialog

--- src/editor/NewLabyrinthController.java
+++ src/editor/NewLabyrinthController.java
@@ -1,13 +1,24 @@
 package editor;
 
 import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 
 public class NewLabyrinthController {
+
+    @FXML private GridPane gridPane;
 
     /**
      * Close the application when the exit menu item is clicked.
      */
     public void handleExitMenuItem() {
         Platform.exit();
+    }
+
+    public void buttonPushed(){
+        FileChooser fc = new FileChooser();
+        fc.setTitle("RESOURCES");
+        fc.showOpenDialog(gridPane.getScene().getWindow());
     }
 }
