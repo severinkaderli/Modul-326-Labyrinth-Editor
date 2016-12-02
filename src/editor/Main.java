89204fe646fa5db2b2bebf15bf7013f1336ea2c1
Marius Schär <schaer.marius@gmail.com>
2016-12-02 17:25:09 +0100

    "fuck user-friendly design, this is easier" - a desperate programmer

--- src/editor/Main.java
+++ src/editor/Main.java
@@ -8,11 +8,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/editor.fxml"));
         primaryStage.setTitle("Labyrinth Editor");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        //to hell with user friendliness, responsive design is hard.
+        primaryStage.setMaxHeight(HEIGHT);
+        primaryStage.setMaxWidth(WIDTH);
+        primaryStage.setMinHeight(HEIGHT);
+        primaryStage.setMinWidth(WIDTH);
+        //TODO make more user friendly, this is not a top priority however.
         primaryStage.show();
     }
 
