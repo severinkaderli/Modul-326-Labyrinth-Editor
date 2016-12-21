536e5e4ef22ccbddbf9784d4f657e2a136e862d7
Severin Kaderli <severin.kaderli@hotmail.com>
2016-12-21 13:45:21 +0100

    Validate that a labyrinth has four spawn points
    Signed-off-by: Severin Kaderli <severin.kaderli@hotmail.com>

--- src/editor/controllers/EditorController.java
+++ src/editor/controllers/EditorController.java
@@ -14,6 +14,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
@@ -126,10 +127,19 @@ public class EditorController {
      */
     public void handleSaveMenuItem() {
         if (currentFile != null) {
-            // TODO: Validate the labyrinth before saving
 
-            // Save the labyrinth in file
-            LabyrinthExporter.exportXML(labyrinth, currentFile);
+            // Validate that the labyrinth has exactly four spawn points
+            if(labyrinth.getNumberOfSpawnpoints() != 4) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("A labyrinth needs exactly four spawnpoints!");
+                alert.setHeaderText("A labyrinth needs exactly four spawnpoints!");
+                alert.showAndWait();
+            } else {
+                // Save the labyrinth in file
+                LabyrinthExporter.exportXML(labyrinth, currentFile);
+            }
+
+
         } else {
             System.out.println("No currentFile");
         }
--- src/editor/models/Labyrinth.java
+++ src/editor/models/Labyrinth.java
@@ -123,6 +123,19 @@ public class Labyrinth {
         return data;
     }
 
+    public int getNumberOfSpawnpoints() {
+        int numberOfSpawnsPoints = 0;
+
+        for(int x = 0; x < getWidth(); x++) {
+            for(int y = 0;  y < getHeight() ; y++) {
+                if(getData().get(x).get(y) instanceof SpawnPoint) {
+                    numberOfSpawnsPoints++;
+                }
+            }
+        }
+        return numberOfSpawnsPoints;
+    }
+
     /**
      * Method that takes width and height to initialize the data array
      * @param width
